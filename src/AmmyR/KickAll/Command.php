1f806a638109ef3f6a0bbe12be0e84609e1fa84a
AmmyR <63977585+AmmyR@users.noreply.github.com>
2020-04-19 16:41:10 -0500

    Add files via upload

---
+++ src/AmmyR/KickAll/Command.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace AmmyR\KickAll;
+
+use pocketmine\command\{Command as Cmd, CommandSender};
+use pocketmine\utils\Config;
+use pocketmine\Player;
+use AmmyR\KickAll\Main;
+
+class Command extends Cmd {
+
+	private $main;
+
+	public function __construct(Main $main){
+		$this->p = $main;
+		parent::__construct("kickall", "Kick all the online players!", "/kickall <string: message>");
+		$this->setPermission("kickall.command");
+	}
+
+	public function execute(CommandSender $player, string $commandLabel, array $args) : void{
+		if(!$player instanceof Player){
+			if($args == ""){
+				if($this->p->getServer()->getOnlinePlayers() == null){
+					$this->p->getServer()->getLogger()->info("[KickAll] - There are not any players to kick!");
+				}else{
+					foreach($this->p->getServer()->getOnlinePlayers() as $players){
+						$players->kick("", false);
+					}
+					$this->p->getServer()->getLogger()->info("[KickAll] - All players kicked!");
+				}
+			}else{
+				if($this->p->getServer()->getOnlinePlayers() == null){
+					$this->p->getServer()->getLogger()->info("[KickAll] - There are not any players to kick!");
+				}else{
+					foreach($this->p->getServer()->getOnlinePlayers() as $players){
+						$players->kick(implode(" ", $args), false);
+					}
+					$this->p->getServer()->getLogger()->info("[KickAll] - All players kicked!");
+				}
+			}
+		}else{
+			if(!$this->testPermission($player)) return;
+			if($args == ""){
+				foreach($this->p->getServer()->getOnlinePlayers() as $players){
+					$players->kick("", true);
+				}
+			}else{
+				foreach($this->p->getServer()->getOnlinePlayers() as $players){
+					$players->kick(implode(" ", $args), true);
+				}
+			}
+			$this->p->getServer()->getLogger()->info("[KickAll] - All players kicked by \"" . $player->getName(). "\"!");
+		}
+	}
+}
--- src/AmmyR/KickAll/Main.php
+++ src/AmmyR/KickAll/Main.php
@@ -1,1 +1,15 @@
+<?php
 
+namespace AmmyR\KickAll;
+
+use pocketmine\plugin\PluginBase;
+use AmmyR\KickAll\Command;
+
+class Main extends PluginBase {
+
+	public function onEnable() : void{
+		$this->getServer()->getCommandMap()->register("kickall", new Command($this));
+		$this->getServer()->getLogger()->info("- §aKickAll by AmmyR enabled successfully. §b(Twitter: @AmmyRQ)");
+	}
+
+}
